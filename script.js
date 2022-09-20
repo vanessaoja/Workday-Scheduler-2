de221e26c20f46b117f2096dcdc899bb1f5c1690
vanessaoja <ojavanessa@gmail.com>
2022-09-20 13:39:31 -0500

    parse

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ var today =
     moment().format("dddd MMMM Do YYYY");
 
 document.getElementById("currentDay").append(today);
+
+// set current hour in a variable
+var currentHour = moment().hour();
+console.log(currentHour);
+
+var setUpHourBlock = function (hourBlock) {
+    console.log(hourBlock)
+
+    // parse hourblock div id into a number
+    var hourId = Number.parseInt(hourBlock.id)
+    console.log(hourId);
